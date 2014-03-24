6770071f42ed281b2dff3ef4a6e1d95129e58c45
Benjamin Moss <drteeth@gmail.com>
2014-03-24 12:47:40 -0400

    add puma socket location

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,8 @@
 threads 8,32
 workers 3
 preload_app!
+
+# pidfile "/tmp/puma/puma.pid"
+# state_path "/tmp/puma/puma.state"
+bind "unix:///sites/indie88.monkeyhouse.ca/sockets/puma.sock"
+# environment "production"
