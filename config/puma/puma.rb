9f6018db0efae6a1de450a3c58bb2b60eca6aceb
Benjamin Moss <drteeth@gmail.com>
2014-03-25 22:04:02 -0400

    fix puma state and pid paths.

--- config/puma/puma.rb
+++ config/puma/puma.rb
@@ -2,7 +2,7 @@ threads 8,32
 workers 3
 preload_app!
 
-pidfile "/tmp/puma/puma.pid"
-state_path "/tmp/puma/puma.state"
+pidfile "/sites/indie88.monkeyhouse.ca/shared/tmp/puma/puma.pid"
+state_path "/sites/indie88.monkeyhouse.ca/shared/tmp/puma/puma.state"
 bind "unix:///sites/indie88.monkeyhouse.ca/shared/sockets/puma.sock"
 environment "production"
