3e6d2c7a2334f37fe77880e825da82a9bb874e09
Benjamin Moss <drteeth@gmail.com>
2014-09-05 11:13:01 -0400

    remove puma state stuff from config as the server will have it.

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,3 @@
 threads 8,32
 workers 3
 preload_app!
-
-pidfile "/sites/indie88.monkeyhouse.ca/shared/tmp/puma/puma.pid"
-state_path "/sites/indie88.monkeyhouse.ca/shared/tmp/puma/puma.state"
-bind "unix:///sites/indie88.monkeyhouse.ca/shared/sockets/puma.sock"
-environment "production"
